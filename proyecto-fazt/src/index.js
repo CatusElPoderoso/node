7c264a11f27a221bcc73cacac96b10a4f6242415
CatusElPoderoso <cactuselpoeroso@gmail.com>
2024-08-09 14:34:05 -0600

    servidor

--- proyecto-fazt/src/index.js
+++ proyecto-fazt/src/index.js
@@ -1,1 +1,19 @@
-console.log("servidor cacacaca");
+//  --creamos el servidor con express
+const express = require('express');
+//  --donde la constate app es el servidor
+const app = express();
+
+// SETTINGS
+//  --establecemos el puerto en el que se aloja la aplicacion
+app.set('port', 8080);
+
+// MIDDLEWARES
+
+// ROUTES
+
+// STATIC FILES
+
+// LISTENING SERVER
+app.listen (app.get('port'), () => {
+    console.log('Servidor corriendo en el puerto', app.get('port'));
+})  //  --el servidor escucha en el puerto 8080
