fcfc49a5f58d3dd149684760e75faf88875cbdce
CatusElPoderoso <cactuselpoeroso@gmail.com>
2024-08-09 15:11:58 -0600

    new view 'almacenes'

--- proyecto-fazt/src/routes/index.js
+++ proyecto-fazt/src/routes/index.js
@@ -5,4 +5,8 @@ router.get('/proyecto-fazt', (req, res) => {
     res.render('index', {title: "Pagina web"});
 });
 
+router.get('/proyecto-fazt/almacen', (req, res) => {
+    res.render('almacen', {title: "Almacenes"});
+});
+
 module.exports = router;
