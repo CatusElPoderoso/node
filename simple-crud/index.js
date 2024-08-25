c871fd3efc070db28fee5ae58a8401759f3bc66e
CatusElPoderoso <cactuselpoeroso@gmail.com>
2024-08-24 22:15:15 -0600

    express & express settings

--- simple-crud/index.js
+++ simple-crud/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+
+// SERVIDOR CON EXPRESS
+const express = require('express');
+const app = express();
+
+
+// SETTINGS
+//  --establecemos el puerto en el que se aloja la aplicacion
+app.set('port', 8080);
+//  --nombre de la aplicacion
+const app_name = 'simple-crud';
+
+
+// LISTENING SERVER
+app.listen (app.get('port'), () => {
+    console.log('http://localhost:'+app.get('port')+'/'+app_name);
+})  //  --el servidor escucha en el puerto 8080
+
